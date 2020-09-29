c7d492ad4092854d23e779a63218015a7ed70158
Kwasi <kattahkwasi@xs4all.nl>
2020-09-29 10:11:14 +0200

    added winforms

---
+++ GimpiesConsole/Program.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using Microsoft.VisualBasic.CompilerServices;
+
+namespace GimpiesConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string defaultTitle = "Gimpies Voorraad systeem | Developed by: Kwasidk";
+            Console.Title = defaultTitle ;
+            
+            string[] userCredential = {"Beheerder", "Intern", "Inkoop"};
+            string[] passwordCredential = {"Beheerder01", "Intern01", "Inkoop01"};
+            
+            int loginCounter = 3;
+
+            bool hasAccess;
+            
+            
+            Console.WriteLine(defaultTitle);
+            Console.WriteLine(" ");
+
+            while (true)
+            {
+                Console.WriteLine("Voer uw gebruikersnaam in: ");
+
+                string userLogin = Console.ReadLine();
+
+                Console.WriteLine("Voer uw wachtwoord in:");
+
+                string userPassword = GetPassword();
+
+                if (userLogin == userCredential[2]  && userPassword == passwordCredential[2])
+                {
+                    EnviromentInkoop();
+                    hasAccess = true;
+                }
+                else
+                {
+                    Console.Clear();
+                    loginCounter--;
+                    Console.WriteLine("Gegevens kloppen niet, probeer het opnieuw.");
+                    Console.WriteLine("U heeft " + loginCounter + " poging(en)!");
+                    Console.WriteLine("");
+                }
+
+                if (loginCounter == 0)
+                {
+                    Console.WriteLine("Je hebt 3 pogingen gehad, probeer het later opnieuw!");
+                    Console.ReadKey();
+                    Environment.Exit(0);
+                }
+            }
+        }
+
+        public static string GetPassword()
+        {
+            string userPassword = String.Empty;
+            ConsoleKey key;
+            do
+            {
+                var keyInfo = Console.ReadKey(intercept: true);
+                key = keyInfo.Key;
+
+                if (key == ConsoleKey.Backspace && userPassword.Length > 0)
+                {
+                    Console.Write("\b \b");
+                    userPassword = userPassword[0..^1];
+                }
+                else if (!char.IsControl(keyInfo.KeyChar))
+                {
+                    Console.Write("*");
+                    userPassword += keyInfo.KeyChar;
+                }
+            } while (key != ConsoleKey.Enter);
+
+            return userPassword;
+        }
+
+        public static void EnviromentInkoop()
+        {
+            Console.Clear();
+            
+            Console.WriteLine("Ingelogd als Inkoop");
+            
+            Console.WriteLine("1: Naar schoenen overzicht");
+            Console.WriteLine("2: Uitloggen ");
+
+            int keuzeMenu = Convert.ToInt32(Console.ReadLine());
+            switch (keuzeMenu)
+            {
+                case 1:
+                    string[] Nike = new string[4]
+                    {
+                        "1",
+                        "Nike",
+                        "Air Force 1",
+                        "0"
+                    };
+                    
+                    string[] adidas = new string[4]
+                    {
+                        "2",
+                        "Adidas",
+                        "Ultraboost",
+                        "6"
+                    };
+
+                    ShowOverviewShoes(Nike, adidas);
+                    break;
+                case 2:
+                    Console.WriteLine("Fijne dag verder!");
+                    Environment.Exit(0);
+                    break;
+                default:
+                    Console.WriteLine("Invalid input");
+                    EnviromentInkoop();
+                    break;
+            }
+            
+        }
+
+        public static void ShowOverviewShoes(string [] shoe1 , string [] shoe2)
+        {
+            // Console.WriteLine("Haha prank neef!");
+            // Console.ReadKey();
+
+
+            if (Convert.ToInt32(shoe1[3]) < 5)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+            } else if (Convert.ToInt32(shoe2[3]) < 5)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+            }
+            
+            Console.WriteLine("ID: " + shoe1 [0] + " Merk: " + shoe1[1] + " Model: " + shoe1[2] + " Aantallen: " + shoe1[3] );
+            Console.WriteLine("");
+            Console.WriteLine("ID: " + shoe2 [0] + " Merk: " + shoe2[1] + " Model: " + shoe2[2] + " Aantallen: " + shoe2[3] );
+            Console.ReadKey();
+
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+    }
+}
---
+++ GimpiesWinForms/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace GimpiesWinForms
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.Button LoginButton;
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            LoginButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // LoginButton
+            // 
+            LoginButton.Location = new System.Drawing.Point(213, 344);
+            LoginButton.Name = "LoginButton";
+            LoginButton.Size = new System.Drawing.Size(286, 68);
+            LoginButton.TabIndex = 0;
+            LoginButton.Text = "Login";
+            LoginButton.UseVisualStyleBackColor = true;
+            LoginButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(189, 219);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(348, 31);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(189, 276);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.PasswordChar = '*';
+            this.textBox2.Size = new System.Drawing.Size(348, 31);
+            this.textBox2.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(12, 222);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(170, 24);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Gebruikersnaam";
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(12, 279);
+            this.label2.Name = "label2";
+            this.label2.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.label2.Size = new System.Drawing.Size(132, 24);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Wachtwoord";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(770, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(LoginButton);
+            this.Name = "Form1";
+            this.Text = "Gimpies Voorraadbeheer ";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+
+        #endregion
+    }
+}
